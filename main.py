359cca9afa8e066504925254629152ab7fb06b95
David Lawrie <dlawrie@att.net>
2019-01-20 09:33:05 -0600

    added add-confirmation.html

--- main.py
+++ main.py
@@ -50,12 +50,12 @@ def add_movie():
         return redirect("/?error=" + error)
 
     # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
-    new_movie_escaped = cgi.escape(new_movie, quote=True)
+     #new_movie_escaped = cgi.escape(new_movie, quote=True)
 
     # TODO:
     # Create a template called add-confirmation.html inside your /templates directory
     # Use that template to render the confirmation message instead of this temporary message below
-    return "Confirmation Message Under Construction..."
+    return render_template('add-confirmation.html', new_movie=new_movie)
 
 # TODO:
 # Modify the edit.html file to display the watchlist in an unordered list with bullets in front of each movie.
